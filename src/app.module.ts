0e9d2c665dcb1da9793afd302d5d3c0f827790cb
Emerson Vieira <emevieira@gmail.com>
2024-01-23 16:04:35 -0400

    feat: implementacao do prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/modules/courses.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
